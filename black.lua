18f5fbef6ee1827f215135cf695cec068f48e636
Alhmirey1 <76887350+Alhmirey1@users.noreply.github.com>
2022-08-09 03:06:11 +0300

    update black.lua

--- black.lua
+++ black.lua
@@ -17924,17 +17924,16 @@ local id = xd[2]
 if tonumber(IdUser) == tonumber(UserId) then
 local u = LuaTele.getUser(IdUser)
 LuaTele.answerCallbackQuery(data.id, "※ انتظر يتم التحميل ", true)
-local url = io.popen('curl -s "https://xnxx.fastbots.ml/infovd.php?id='..id..'"'):read('*a')
+local url = io.popen('curl -s "https://moh-yen.org/api/youtube.php?url='..id..'"'):read('*a')
 local json = JSON.decode(url)
-local link = "https://www.youtube.com/watch?v="..id
+local link = json.url
 local title = json.title
 local title = title:gsub("/","-") 
 local title = title:gsub("\n","-") 
 local title = title:gsub("|","-") 
 local title = title:gsub("'","-") 
 local title = title:gsub('"',"-") 
-local time = json.t
-local p = json.a
+local p = json.p
 local p = p:gsub("/","-") 
 local p = p:gsub("\n","-") 
 local p = p:gsub("|","-") 
@@ -17942,7 +17941,7 @@ local p = p:gsub("'","-")
 local p = p:gsub('"',"-") 
 LuaTele.deleteMessages(ChatId,{[1]= Msg_id})
 os.execute("yt-dlp "..link.." -f 251 -o '"..title..".mp3'")
-LuaTele.sendAudio(ChatId,0,'./'..title..'.mp3',"※ ["..title.."]("..link..")\n※ حسب طلب ["..u.first_name.."](tg://user?id="..IdUser..")","md",tostring(time),title,p) 
+LuaTele.sendAudio(ChatId,0,'./'..title..'.mp3',"※ ["..title.."]("..link..")\n※ حسب طلب ["..u.first_name.."](tg://user?id="..IdUser..")","md",nil,title,p) 
 sleep(2)
 os.remove(""..title..".mp3")
 else
