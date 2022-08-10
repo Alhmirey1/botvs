f25545ac7426cd9f8ff3942b0f74fb339674d84b
Alhmirey1 <76887350+Alhmirey1@users.noreply.github.com>
2022-08-10 05:16:14 +0300

    update black.lua

--- black.lua
+++ black.lua
@@ -17908,8 +17908,9 @@ end
 if data.The_Controller == 1 or data.The_Controller == 2 or data.The_Controller == 3 or data.The_Controller == 44 or data.The_Controller == 4 or data.The_Controller == 5 or data.The_Controller == 6 or data.The_Controller == 7 or data.The_Controller == 8 or data.The_Controller == 9 then
 data.Special = true
 end
-if text:match("^تحميل (.*)$") then
-local id = text:match("^تحميل (.*)$")
+
+if text:match("^ت (.*)$") then
+local id = text:match("^ت (.*)$")
 local title = youtube_api_url(id)
 local reply_markup = LuaTele.replyMarkup{
 type = 'inline',
